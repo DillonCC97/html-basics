03bf4a3e383e3a43f1fff4ac3047816433929343
dilloncc97 <dillonculp51@gmail.com>
2017-03-23 17:03:15 -0400

    finish up outline of jquery project

--- jquery/assignment.js
+++ jquery/assignment.js
@@ -1,1 +1,68 @@
-// Create variable here etc.
+// First add jQuery to your HTML code via CDN
+// Write a variable "body" using jQuery referencing the body of the page
+
+// Write a variable "headline" using jQuery referencing the News Headline
+
+// Write a variable "paragraphs" using jQuery referencing all paragraphs
+
+// Write a variable "subHead" using jQuery referencing the subheading of article authors
+
+// Write variables to store the initial color of "body", "headline", "paragraphs", and "subHead"
+
+
+
+
+// Write a function nightMode() that will
+// 1) Change the background of the page to #141311
+// 2) Change the font color of all paragraphs to #b7b7b6
+// 3) Change the font color of the headline to #ffffff
+// 4) Change the font color of the authors to #a2482e
+
+
+
+
+// Write a function dayMode() that reverts "body", "headline", "paragraphs", and "subHead" to their initial colors
+
+
+
+
+// Add an event listener that runs nightMode() when the logo is clicked
+
+// Add an event listener that runs dayMode() when the user presses the escape button
+
+// Write a function setSummary(text) that changes the summary text of the article to the function input
+
+// Write a function insertList() that changes the 3rd paragraph on the page to a <ul> of lorem ipsum lines
+
+// Write a line of jQuery to give the first paragraph on the page the class "lead"
+
+
+
+var distractionsHidden = false;
+
+// Write a function hideDistractions() that:
+// 1) Has all images slide down and hide
+// 2) Has the navbar slide up and hide
+// 3) Has the footer and all <hr> elements fade out and hide
+// 4) Sets distractionsHidden to true
+
+// Write a function showDistractions() that fades in all elements (images, navbar, footer, hr) hidden by hideDistractions() if distractionsHidden is true.
+
+// Write an event listener that runs hideDistractions() if the user types "no distractions" into the code input on the navbar
+
+// Write an event listener that runs showDistractions() if the user presses enter.
+
+// Write a function changePic() that changes the header image to https://mfour.com/wp-content/uploads/2017/03/gorilla-900x300.jpg
+
+// Write an event listener that runs changePic() when the user hovers over the header image
+
+// Write a function postComment() that:
+// 1) Posts text inside the comment box as a comment below all other comments if there is text inside the comment box
+// 2) Clears the comment box
+
+
+// Write an event listener that runs postComment() when the user presses the Submit button
+
+// Write a function deleteComment() that removes a comment from the dom
+
+// Write an event listener that runs deleteComment on the comment whos X icon is clicked
